ffb67af48588fbf2b429eca5f1369298cc2d0957
Hasina-Mlbb <hasinarakotondrasoa63@gmail.com>
2026-06-10 10:08:16 +0300

    first commit

---
+++ test.java
@@ -0,0 +1,28 @@
+class voiture{
+    String name;
+    String marque;
+
+    public voiture(String name,String marque){
+        this.name=name;
+        this.marque=marque;
+    }
+}
+class eleve{
+    String name;
+
+    public note(int note1,int note2,int note3,int note4,int note5){
+        int*releve;
+        for(int i=0;i<5;i++){
+            this.releve[i]=
+        }
+    }
+}
+public class test{
+    public static void main(String args[]){
+
+        voiture mavoiture=new voiture("Ferrari","F488");
+
+        System.out.println("hello");
+        System.out.println(mavoiture.name);
+    }
+}
